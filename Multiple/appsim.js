a3812098bea2e53f32f0fefab31edac35446635b
morosainos <morosainos@163.com>
2016-12-21 00:14:39 +0800

    Add files via upload

---
+++ Multiple/appsim.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+angular.module('ghostapp', [
+	'angular-simditor'
+])
+.config(['simditorConfig',function(simditorConfig) {
+	simditorConfig.placeholder = '这里输入文字...';
+}])
+.controller('TestCtrl', ['$scope', function($scope){
+	$scope.test = 'test content';
+}]);
